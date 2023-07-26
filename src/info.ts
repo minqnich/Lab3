331b2450ebd1994e588c2c1199e879e5a1437c03
nichakamol <nichakamol_sanghong@cmu.ac.th>
2023-07-27 04:45:50 +0700

    first commit

---
+++ src/info.ts
@@ -0,0 +1,10 @@
+export interface studentInfo{
+    id: number
+    studentId: string
+    name: string
+    surname: string
+    gpa: string
+    image: string
+    penAmount: string
+    description: string
+}
